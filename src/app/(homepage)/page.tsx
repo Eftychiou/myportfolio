a0bd155c3804ef7942fc36c28cc7c587778ee6b5
Giorgos Eftichiou <eftichiou@hotmail.com>
2023-12-12 12:46:32 +0200

    About me Centered

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -21,9 +21,7 @@ export default function Home() {
         </div>
       </div>
 
-      <section id='about_me'>
-        <h1>About Me</h1>
-        <h3>FULL STACK DEVELOPER</h3>
+      <section id='about_me' className={classes.about_me}>
         <div className={classes.image_wrapper}>
           <Image
             src='/images/profile_1.jpg'
@@ -34,24 +32,31 @@ export default function Home() {
             sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
           />
         </div>
-        <p>
-          In 2019, I embarked on a transformative journey into the dynamic realm of web development. Fueled by a passion
-          for crafting immersive digital experiences, my expertise revolves around harnessing the power of React,
-          JavaScript, CSS, and Node.js. I thrive on the intricate dance of code, consistently pushing boundaries and
-          elevating user interfaces to new heights.
-        </p>
+        <div>
+          <div className={classes.heading}>
+            <h1>About Me</h1>
+            <h3>FULL STACK DEVELOPER</h3>
+          </div>
 
-        <p>
-          Beyond the web, I delve into the expansive world of Java, seamlessly weaving through Android and Spring
-          frameworks to bring my ideas to life. My journey extends beyond mere technical mastery; its a canvas where
-          creativity meets functionality.
-        </p>
+          <p>
+            In 2019, I embarked on a transformative journey into the dynamic realm of web development. Fueled by a
+            passion for crafting immersive digital experiences, my expertise revolves around harnessing the power of
+            React, JavaScript, CSS, and Node.js. I thrive on the intricate dance of code, consistently pushing
+            boundaries and elevating user interfaces to new heights.
+          </p>
 
-        <p>
-          As I navigate this ever-evolving landscape, I am not just a developer; I am a creator weaving lines of code
-          into digital tapestries. Join me on this exciting venture, where innovation meets expression, and each project
-          is a testament to my commitment to excellence.
-        </p>
+          <p>
+            Beyond the web, I delve into the expansive world of Java, seamlessly weaving through Android and Spring
+            frameworks to bring my ideas to life. My journey extends beyond mere technical mastery; its a canvas where
+            creativity meets functionality.
+          </p>
+
+          <p>
+            As I navigate this ever-evolving landscape, I am not just a developer; I am a creator weaving lines of code
+            into digital tapestries. Join me on this exciting venture, where innovation meets expression, and each
+            project is a testament to my commitment to excellence.
+          </p>
+        </div>
       </section>
 
       <section id='resume'>
