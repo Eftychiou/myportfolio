de46036a056b9b8810c57083fe0f5adf384c0242
Giorgos Eftichiou <eftichiou@hotmail.com>
2024-09-28 18:03:01 +0300

    New Cv

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -143,12 +143,12 @@ export default function Home() {
   const downloadFile = () => {
     // Create a Blob with the file content
     const blob = new Blob([''], { type: 'application/pdf' });
-    const url = '/cv.pdf';
+    const url = '/George-Eftichiou-CV.pdf';
 
     // Create a temporary <a> element to initiate the download
     const a = document.createElement('a');
     a.href = url;
-    a.download = 'cv.pdf';
+    a.download = 'George-Eftichiou-CV.pdf';
 
     // Append the <a> element to the document body
     document.body.appendChild(a);
