63ab5476cf1ceea193fe4ddde3ef8be934081618
Giorgos Eftichiou <eftichiou@hotmail.com>
2024-10-02 23:23:33 +0300

    Test webhook 8

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -172,7 +172,7 @@ export default function Home() {
 
         <div className={classes.description_container}>
           <div className={classes.header_title}>
-            <p>FULL STACK DEVELOPER 7</p>
+            <p>FULL STACK DEVELOPER 8</p>
           </div>
           <div className={classes.description}>
             <div className={classes.image}>
