2aaba45a04af3a35d588b66d27993fab097e7179
Giorgos Eftichiou <eftichiou@hotmail.com>
2025-06-27 11:53:24 +0300

    env variables update

--- src/app/(homepage)/_actions/poke.ts
+++ src/app/(homepage)/_actions/poke.ts
@@ -1,8 +1,8 @@
 'use server';
 
 export async function sayHello(ip: string) {
-  const phoneNumber = process.env.CALLMEBOT_PHONE_NUMBER;
-  const apiKey = process.env.CALLMEBOT_API_KEY;
+  const phoneNumber = process.env.NEXT_PUBLIC_CALLMEBOT_PHONE_NUMBER;
+  const apiKey = process.env.NEXT_PUBLIC_CALLMEBOT_API_KEY;
   const message = encodeURIComponent(`Someone with ip ${ip} is saying sending his greetings`);
 
   const url = `https://api.callmebot.com/whatsapp.php?phone=${phoneNumber}&text=${message}&apikey=${apiKey}`;
