e611737ce23e7250d58ef01d0d51f4610459e789
Giorgos Eftichiou <eftichiou@hotmail.com>
2025-06-27 12:14:12 +0300

    env variables update _

--- src/app/(homepage)/_actions/poke.ts
+++ src/app/(homepage)/_actions/poke.ts
@@ -1,12 +1,18 @@
 'use server';
 
 export async function sayHello(ip: string) {
+  const sanitizedIp = ip.replace(/[^a-zA-Z0-9]/g, '_');
+
   const phoneNumber = process.env.NEXT_PUBLIC_CALLMEBOT_PHONE_NUMBER;
   const apiKey = process.env.NEXT_PUBLIC_CALLMEBOT_API_KEY;
-  const message = encodeURIComponent(`Someone with ip ${ip} is saying sending his greetings`);
+  const message = encodeURIComponent(`Someone with ip ${sanitizedIp} is saying sending his greetings`);
 
   const url = `https://api.callmebot.com/whatsapp.php?phone=${phoneNumber}&text=${message}&apikey=${apiKey}`;
 
-  await fetch(url);
+  fetch(url)
+    .then((res) => {
+      return res.text();
+    })
+    .catch((err) => console.log(err));
   return 'Greetings was send to George';
 }
