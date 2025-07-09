bc2032da624faf977c56126100a833c9735ea32c
Giorgos Eftichiou <eftichiou@hotmail.com>
2025-07-10 00:18:36 +0300

    normal

--- src/app/(homepage)/_components/IFrameGame/IFrameGame.tsx
+++ src/app/(homepage)/_components/IFrameGame/IFrameGame.tsx
@@ -3,31 +3,30 @@ import { useEffect, useState } from 'react';
 import classes from './IFrameGame.module.scss';
 
 export const IFrameGame = () => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
   const [isMobile, setIsMobile] = useState(true);
 
-  // useEffect(() => {
-  //   const mediaQuery = window.matchMedia('(max-width: 900px)');
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 900px)');
 
-  //   const handleChange = (e: MediaQueryListEvent) => {
-  //     setIsMobile(e.matches);
-  //   };
+    const handleChange = (e: MediaQueryListEvent) => {
+      setIsMobile(e.matches);
+    };
 
-  //   handleChange(mediaQuery as unknown as MediaQueryListEvent);
+    handleChange(mediaQuery as unknown as MediaQueryListEvent);
 
-  //   mediaQuery.addEventListener('change', handleChange);
-  //   return () => mediaQuery.removeEventListener('change', handleChange);
-  // }, []);
+    mediaQuery.addEventListener('change', handleChange);
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, []);
 
   return (
-    // style={{ display: isMobile ? 'none' : 'block' }}
-    <div className={classes.IFrameGame}>
+    <div className={classes.IFrameGame} style={{ display: isMobile ? 'none' : 'block' }}>
       <div className={classes.toggle} onClick={() => setShow((state) => !state)}>
         CHAT
       </div>
 
       <iframe
-        // className={show ? classes.visible : classes.not_visible}
+        className={show ? classes.visible : classes.not_visible}
         // style={{ display: show ? 'block' : 'none' }}
         src={process.env.NEXT_PUBLIC_IFRAME_URL}
         width={800}
